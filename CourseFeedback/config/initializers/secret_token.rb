62d648645d7f23d9e1c181c35172c5d15117842a
Rohit Alekar <rohit.alekar@hotmail.com>
2012-11-03 14:14:14 -0400

    Changing gemfile

---
+++ CourseFeedback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseFeedback::Application.config.secret_token = 'b886d4997f9122bb1ab2e2260c5cbbc96dd94b365e6fa058e944d183cf24b2ea78a002431d950225087b0babc2a45acd5fc6e0c336c64c61097c964e917863f8'
